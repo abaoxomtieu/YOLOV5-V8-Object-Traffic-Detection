b6b191cc5bbba394eef8f565e8f16751b458db4c
hotonbao <htbqn2003@gmail.com>
2023-10-26 15:59:18 +0700

    use realtime with api and streamlit

--- api.py
+++ api.py
@@ -6,9 +6,21 @@ import numpy as np
 from main import *
 from utils import load_session
 from preprocess import resize_and_pad
-from fastapi.responses import FileResponse
+from fastapi.responses import FileResponse, StreamingResponse
 import uvicorn
 import tempfile
+
+
+
+# Define the class labels
+IDX2TAGs = {
+  0: "bicycle",
+  1: "bus",
+  2: "car",
+  3: "motorbike",
+  4: "person"
+}
+
 api = FastAPI()
 
 class CFG:
@@ -18,6 +30,46 @@ class CFG:
 
 cfg = CFG()
 session = load_session(PATH_MODEL)
+
+def generate_frames():
+    cap = cv2.VideoCapture(0)
+
+    while True:
+        ret, frame = cap.read()
+
+        if not ret:
+            break
+
+        # Resize and pad the frame
+        image, ratio, (padd_left, padd_top) = resize_and_pad(frame, new_shape=cfg.image_size)
+        img_norm = normalization_input(image)
+
+        # Run inference and post-process
+        pred = infer(session, img_norm)
+
+        if pred is not None:
+            pred = postprocess(pred)[0]
+
+            # Annotate the frame with bounding boxes and labels
+            for box in pred:
+                x1, y1, x2, y2, confidence, class_idx = box
+                x1, y1, x2, y2 = map(int, [x1, y1, x2, y2])
+                class_label = IDX2TAGs.get(int(class_idx), "Unknown")
+                label = f"{class_label}: {confidence:.2f}"
+                print("label")
+                # Apply padding and ratio adjustments to each bounding box
+                x1 = int((x1 - padd_left) / ratio)
+                y1 = int((y1 - padd_top) / ratio)
+                x2 = int((x2 - padd_left) / ratio)
+                y2 = int((y2 - padd_top) / ratio)
+
+                cv2.rectangle(frame, (x1, y1), (x2, y2), (0, 255, 0), 2)
+                cv2.putText(frame, label, (x1, y1 - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 255, 0), 2)
+
+        _, buffer = cv2.imencode('.jpg', frame)
+        frame_bytes = buffer.tobytes()
+        yield (b'--frame\r\n'
+               b'Content-Type: image/jpeg\r\n\r\n' + frame_bytes + b'\r\n')
 
 
 @api.post("/predict/")
@@ -46,3 +98,10 @@ async def predict(file: UploadFile):
         temp_file_path = temp_file.name
 
     return FileResponse(temp_file_path, media_type="image/jpeg")
+@api.get('/')
+async def get():
+    return StreamingResponse(generate_frames(), media_type="multipart/x-mixed-replace; boundary=frame")
+
+if __name__ == '__main__':
+    import uvicorn
+    uvicorn.run(api, host='127.0.0.1', port=8000)
--- app.py
+++ app.py
@@ -1,34 +1,42 @@
 import streamlit as st
 import requests
-from PIL import Image
 from io import BytesIO
+from PIL import Image
 
-# Define the FastAPI server URL
-fastapi_url = "http://127.0.0.1:8000"  # Update the URL to match your FastAPI server URL
+# Set the FastAPI service URL
+api_url = "http://127.0.0.1:8000"  # Replace with the URL of your FastAPI service
 
-st.title("FastAPI Image Prediction Demo")
+st.title("Image Prediction and Real-Time Object Detection")
 
-# Upload an image for prediction
-uploaded_image = st.file_uploader("Upload an image", type=["jpg", "jpeg", "png"])
+# Use a radio button to choose the service
+selected_service = st.radio("Select a service", ["Image Prediction", "Real-Time Object Detection"])
 
-if uploaded_image is not None:
-    # Display the uploaded image
-    st.image(uploaded_image, caption="Uploaded Image", use_column_width=True)
+if selected_service == "Image Prediction":
+    # Upload an image for prediction
+    uploaded_image = st.file_uploader("Upload an image", type=["jpg", "jpeg", "png"])
 
-    # Make a prediction request to the FastAPI server
-    if st.button("Predict"):
-        try:
-            # Send the image to the FastAPI server for prediction
-            response = requests.post(fastapi_url + "/predict/", files={"file": uploaded_image})
-            response.raise_for_status()
+    if uploaded_image is not None:
+        # Display the uploaded image
+        st.image(uploaded_image, caption="Uploaded Image", use_column_width=True)
 
-            # Display the prediction result
-            result_image = Image.open(BytesIO(response.content))
-            st.image(result_image, caption="Prediction Result", use_column_width=True)
+        # Make a prediction request to the FastAPI server
+        if st.button("Predict"):
+            try:
+                # Send the image to the FastAPI server for prediction
+                response = requests.post(api_url + "/predict/", files={"file": uploaded_image})
+                response.raise_for_status()
 
-        except requests.exceptions.RequestException as e:
-            st.error(f"An error occurred: {e}")
+                # Display the prediction result
+                result_image = Image.open(BytesIO(response.content))
+                st.image(result_image, caption="Prediction Result", use_column_width=True)
+
+            except requests.exceptions.RequestException as e:
+                st.error(f"An error occurred: {e}")
+    else:
+        st.info("Upload an image for prediction.")
 else:
-    st.info("Upload an image for prediction.")
+    # Stream the video feed from the FastAPI service
+    iframe = f'<iframe src="{api_url}" width="1280" height="720"></iframe>'
+    st.markdown(iframe, unsafe_allow_html=True)
 
 st.write("Note: Make sure your FastAPI server is running and accessible at the specified URL.")
